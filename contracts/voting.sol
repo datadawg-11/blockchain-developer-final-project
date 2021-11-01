3fb080673ec33cdff03017f4c7d91560261fe8a9
datadawg-11 <vinci.ind@outlook.com>
2021-11-01 15:29:32 +0700

    Updated base sol file

--- contracts/voting.sol
+++ contracts/voting.sol
@@ -1,46 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.16 <0.9.0;
-
+pragma experimental ABIEncoderV2;
 // import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract voting {
   // State variables
   uint256 totalVotes; // total votes received
-  address public chairPerson;  // address of the contract deployer is chairperson
+  address public chairperson;  // address of the contract deployer is chairperson
   mapping (address => Voter) voters; // mapping of items for sale. So each skuID corresponds to a dictionary
-  Proposal[] public proposals; // initialise an array of proposals
+  Proposal[] public proposals; // initialise an empty array of proposals
   
   struct Voter {
-    address voterAddress; // address of voter
+    // address voterAddress; // address of voter
     uint vote; // the index of the thing voted for
+    // uint voteNumber; // number of votes that voter has given
     string message; // message of the voter
     uint timestamp; // timetamp of the vote
   }
 
   struct Proposal {
-    bytes32 name; // name of the proposal
+    string name; // name of the proposal
     uint voteCount; // number of accumulated votes
   }
 
   enum State {
     Created, Voting, Ended
-  } State public state
+  } State public state;
 
 
   // Events
 // event itemAdded(uint _skuCount, string _productName);
 
   // Modifiers
-modifier enoughItems(uint _inventory) {
-  require(_inventory>=1);
-  _;
-}
-  constructor(bytes32[] memory proposalNames) public {  // constructor initialises once. 
-    chairPerson = msg.sender; // the person who deployed constract is the owner
+// modifier enoughItems(uint _inventory) {
+//   require(_inventory>=1);
+//   _;
+// }
+  constructor(string[] memory proposalNames) public {  // constructor initialises once. 
+    chairperson = msg.sender; // the person who deployed constract is the owner
     
     for (uint i = 0 ; i < proposalNames.length; i++) {
-      proposals.push(Proposals({
-        name: ProposalNames[i],
+      proposals.push(Proposal({
+        name: proposalNames[i],
         voteCount:0
       })); 
       }
@@ -52,24 +53,30 @@ modifier enoughItems(uint _inventory) {
   // 1. Create the voting function
   function vote(string memory _message, uint proposal) public {
     totalVotes +=1; // increment total vote counts
-    console.log("%s has voted!", msg.sender); // create a console log indicating a vote event
-    Voter storage sender = voters[msg.sender]; // 
-    sender.message = _message; 
+    // console.log("%s has voted!", msg.sender); // create a console log indicating a vote event
+    Voter storage sender = voters[msg.sender]; // create new 'Voter' struct named sender within voters mapping
     sender.vote = proposal; 
-    
+    sender.message = _message;  
+    sender.timestamp = block.timestamp;
+
     proposals[proposal].voteCount +=1; 
-  
-
-
   }
-  
+
+  function winningProposal() public view returns (uint winningProposal_) {
+    uint winningVoteCount = 0;
+
+    for (uint p=0; p< proposals.length; p++) {
+      if (proposals[p].voteCount > winningVoteCount) {
+        winningVoteCount = proposals[p].voteCount;
+        winningProposal_ = p;
+      }
+    }
+  }
 
 
-
-
-
-
-
+  function winnerName() public view returns (string memory winnerName_) {
+    winnerName_ = proposals[winningProposal()].name;
+  }
 
 
 
@@ -79,3 +86,20 @@ modifier enoughItems(uint _inventory) {
 
 
 }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

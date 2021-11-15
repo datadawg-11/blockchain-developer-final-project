ccc4e6241068cec13d8599d50ba91d7b068996bd
datadawg-11 <vinci.ind@outlook.com>
2021-11-15 23:13:14 +0700

    4 tests have been implemented so far

--- test/voting.js
+++ test/voting.js
@@ -1,3 +1,6 @@
+// const { assert } = require("console");
+
+
 const voting = artifacts.require("voting");
 
 /*
@@ -6,12 +9,13 @@ const voting = artifacts.require("voting");
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
 contract("voting", function (accounts) {
-  it("voting should assert true for successful deployment", async function () {
+  
+  it("voting should assert true for successful deployment", async () => {
     await voting.deployed();
     return assert.isTrue(true);
   });
 
-  it("voting should have an initial total votes of 0", async function () {
+  it("voting should have an initial total votes of 0", async  () => {
     // get the contract that has been deployed
     const votingInstance = await voting.deployed(); 
 
@@ -21,39 +25,67 @@ contract("voting", function (accounts) {
     assert.equal(_totalVotes, 0, `Initial state of votes should be zero`)
   });
 
+  it("voting should have no voters registered initially", async  () => {
+    // get the contract that has been deployed
+    const votingInstance = await voting.deployed(); 
+
+    // verify it has an initial value of 0
+
+    const _totalRegisteredVoters = await votingInstance.totalVotersRegistered.call() 
+
+    assert.equal(_totalRegisteredVoters,0,"total registered voters initially should be zero")
+  })
+
+  it("voting should have the first proposal name as mason", async () => {
+      // get the contract that has been deployed
+      const votingInstance = await voting.deployed(); 
+
+      const _proposalOne = await votingInstance.proposals[0]["name"];
+      assert.equal(_proposalOne,"mason","Proposal one should be 'mason'")
+
+
+
+
+
+  })
+
+
+
   it("voting should only let the chairperson change the state", async function () {
     // get the contract that has been deployed
     const votingInstance = await voting.deployed(); 
 
     try {
-      await 
+      await votingInstance.startVote({from:accounts[0]}); //for some reason, function with the accounts work
+      console.log('voting success')
     } catch(err) {
+      console.log('error reported')
+     }
 
-    }
+    const currentState = await votingInstance.state.call();
+    // console.log(currentState)
 
-    
-
-
+    assert.equal(currentState,1,"state was not changed")
 
 
   })
 
 
 
-  describe("Functionality", () => {
-      it("should store a new value of 42", async () =>{ 
+  // describe("Functionality", () => {
+  //     it("should store a new value of 42", async () =>{ 
         
-        // grab the contract we need
-        const votingInstance = await voting.deployed();
+  //       // grab the contract we need
+  //       const votingInstance = await voting.deployed();
 
         
 
 
-      })
+  //     })
     
     
     
-  })
+  // })
 
 
   // it("voting should have no initial registered voters", async function () => {
